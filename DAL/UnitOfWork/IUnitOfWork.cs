22499c264023343437483edfdb56415826669d08
irakiw <irakiw03@gmail.com>
2023-01-03 04:03:10 +0200

    Added classes to DAL

---
+++ DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Repositories.Interfaces;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+    }
+}
